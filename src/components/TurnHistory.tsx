d09ffaf5fdf54d808de49eaa2dc365dedfde067a
vladocpro <infernohs9@gmail.com>
2023-03-02 12:57:27 +0000

    small UI redesign

--- src/components/TurnHistory.tsx
+++ src/components/TurnHistory.tsx
@@ -17,8 +17,8 @@ const TurnHistory: FC<TurnHistoryProps> = ({moveHist}) => {
             <div className="turnHistoryLabel">Chess Moves</div>
             <hr className="hrTurnHistory"/>
             <div className="movesContainer" style={{overflowY: moveHist.length > 14 ? "scroll" : "hidden" }}>
-                {moveHist.map(move =>
-                <div key={move.turnCounter} className="movesRow">
+                {moveHist.map((move, index) =>
+                <div key={move.turnCounter} className="movesRow" style={{ borderRadius: index > 13 && moveHist.length -1 === index ? "0 0 0 6px" : ""}}>
                     <span className="historySpan">{move.turnCounter}</span>
                     <div className="historyContainer">
                         {move.figure1?.logo && <img className="historyIcon" src={move.figure1.logo} alt=""/>}
